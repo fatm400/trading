a49b1d6692f9ceee88868e46d94efcdac8e55e90
Fabio Tamburini <fatm400@gmail.com>
2018-03-13 16:09:42 -0400

    codigo para la interrupción de un while infinito a través de una bandera en Base de Datos sqlite - Versión 1

---
+++ connectionDB.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 10:04:47 2018
+
+@author: tamburinifa
+"""
+
+import sqlite3
+
+con = sqlite3.connect('/home/tamburinifa/Git/trading/communication.db')
+
+cursor = con.cursor()
+
+#cursor.execute('INSERT INTO flag (ID, name, flag) VALUES (1, "NANOBTC", 1)')
+
+#con.commit()
+
+#cursor.execute("SELECT ID, name, flag from flag")
+
+while True:
+    print ("estoy en línea")
+    cursor.execute("SELECT flag from flag where ID = 1")
+    registro = cursor.fetchone()
+    if registro[0] == 0:
+        break
+
+#for i in cursor:
+#    print ("ID = ", i[0])
+#    print ("name = ", i[1])
+#    print ("flag = ", i[2], "\n")
+print (registro[0])
+con.close()
